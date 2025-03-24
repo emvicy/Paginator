146e3391dbf8e8f3576751a38c5a8b2a225bfd69
gueff <guido.ueffing@web.de>
2025-03-24 09:26:55 +0100

    update

--- Model/Paginator.php
+++ Model/Paginator.php
@@ -39,7 +39,7 @@ class Paginator
             realpath(__DIR__ . '/../' . '/templates/')
         );
 
-        $iAmountItems = $oDb->count($aDTDBWhere);                   # Number of all items in the requested DB table
+        $iAmountItems = $oDb->count($aDTDBWhere, $aDTDBOption);     # Number of all items in the requested DB table
         $iAmountPages = ceil($iAmountItems / $iMaxProPage);    # Number of individual pagination pages
         $iCurrentPage = (int) get($_GET['p'], 1);                   # current pagination page
 
